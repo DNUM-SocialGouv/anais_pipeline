c9df7414d855162494fecc38ef2222b18c142455
@francois.lebrument <francois.lebrument@seenovate.com>
2025-08-05 17:18:43 +0200

    test: 8

--- pipeline/database_management/duckdb_pipeline.py
+++ pipeline/database_management/duckdb_pipeline.py
@@ -273,7 +273,7 @@ class DuckDBPipeline(DataBasePipeline):
             # Crée la table dans la base cible à partir de la table source
             conn.execute(f"""
                 CREATE TABLE IF NOT EXISTS main.{db_table_name} AS
-                SELECT * FROM staging_db.{staging_table_name}
+                SELECT * FROM staging_db.main.{staging_table_name}
             """)
             conn.execute("DETACH staging_db")
             self.logger.info(f"✅ La table {staging_table_name} a bien été récupérée de la base DuckDB Staging sous le nom {db_table_name}.")
