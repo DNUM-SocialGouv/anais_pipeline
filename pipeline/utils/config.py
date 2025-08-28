a013acf51a41c1fedfacd2209a56316ad4975074
h-titouan <titouan.houde@seenovate.com>
2025-08-28 11:50:19 +0200

    Add: MatricePreciblage to config

--- pipeline/utils/config.py
+++ pipeline/utils/config.py
@@ -10,7 +10,7 @@ from pipeline.utils.logging_management import setup_logger
 # === Constantes ===
 load_dotenv()
 ENV_CHOICE = ["local", "anais"]
-PROFILE_CHOICE = ["Staging", "CertDC", "Helios", "InspectionControlePA", "InspectionControlePH"]
+PROFILE_CHOICE = ["Staging", "CertDC", "Helios", "InspectionControlePA", "InspectionControlePH", "MatricePreciblage"]
 METADATA_YML = "metadata.yml"
 PROFILE_YML = "profiles.yml"
 
